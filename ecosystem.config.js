6f487c336d5d7391d0a8cef34c5429e5293344af
Marc-antoine simon <marc-antoine.simon3@orange.fr>
2021-09-17 13:03:34 +0200

    fix ecosystem bug

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host : '152.228.173.68',
       ref  : 'origin/main',
       repo : 'git@github.com/marcsim/reiki.git',
-      path : '/var/www/my-repository',
+      path : '/var/www/reiki',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
